2d7ae3da151260d7136d3a53e4ef8026782b7751
ahuang3 <andycfa2@163.com>
2018-04-05 15:49:01 +0800

    图标制作

--- numpy 1.py
+++ numpy 1.py
@@ -4,3 +4,18 @@
 # @Email   : andycfa2@163.com
 # @File    : numpy 1.py
 # @Software: PyCharm
+
+import numpy as np
+
+#生成【0,1）之间浮点数
+x = np.random.random_sample(size=(2,2))
+# np.random.random(size=None)
+# np.random.ranf(size=None)
+# np.random.sample(size=None
+
+#设置相同的seed，每次生成的随机数相同
+y = np.random.seed(1)
+y = np.random.rand(5)
+
+
+print (y)
--- pandas 1.py
+++ pandas 1.py
@@ -4,3 +4,69 @@
 # @Email   : andycfa2@163.com
 # @File    : pandas 1.py
 # @Software: PyCharm
+
+
+# pandas 绘图练习
+import matplotlib.pyplot as plt #显示图片
+import pandas as pd
+import numpy as np
+from pylab import mpl #解决中文显示乱码
+
+#设置中文字体
+mpl.rcParams['font.sans-serif'] = ['Microsoft YaHei'] # 指定默认字体：解决plot不能显示中文问题
+mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+
+#读取本地excel文件
+df = pd.read_excel('C://Users//ahuang3//Desktop//test.xlsx')
+
+#使用numpy生成随机数据文件
+df1 = pd.DataFrame(np.random.rand(50,4),columns=['a','b','c','d'])  #dataframe
+ser1 = pd.Series(3*np.random.rand(4),index=['a','b','c','d'],name = 'series')  #列数据
+df2 = pd.DataFrame(3*np.random.rand(4,2),index=['a','b','c','d'],columns = ['x','y'])  #dataframe
+
+
+#绘图参数设置
+
+# df.plot() #无参数默认形式
+
+#柱状图
+# df.plot(kind = 'bar')
+
+
+#散点图
+           #单一散点图
+#df1.plot.scatter(x = 'a',y='b')
+       # 不同散点绘制到一张表
+# ab = df1.plot.scatter(x='a',y='b',color = 'DarkBlue',label = 'Group1')
+# abc = df1.plot.scatter(x='c',y='d',color = 'DarkGreen',label = 'Group2',ax=ab)
+# abcd = df1.plot.scatter(x='a',y='c',color = 'DarkRed',label = 'Group3',ax=abc)
+# df1.plot.scatter(x='b',y='d',color = 'DarkGray',label = 'Group4',ax=abcd)
+          #设置点大小和灰度水平
+# df1.plot.scatter(x='b',y='d',c = 'c',s = 50)
+# df1.plot.scatter(x='a',y='b',s = df1['c']*200)
+
+
+#饼状图
+# ser1.plot.pie(figsize=(6,6)) #单一列
+df2.plot.pie(subplots = True,figsize = (12,6)) #多列数据
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+plt.show()
+
+
